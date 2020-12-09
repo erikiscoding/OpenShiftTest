6b8c6f27a312d8517ece5b0902f246229d70f515
Erik Lewis <61423542+erikiscoding@users.noreply.github.com>
2020-12-09 13:30:25 -0500

    added test code

---
+++ testCode.py
@@ -0,0 +1,7 @@
+your_word = str(input("Enter what word you really really like and enjoy greatly: "))
+
+your_word = your_word.upper()
+
+print("Here is your word but even better: " + your_word)
+print("bye bye now")
+
